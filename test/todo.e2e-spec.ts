f3d396b2b7416877ba16ddf1735d7c522aee44d2
GuilhermeGuitte <guilherme.guitte@gmail.com>
2019-03-02 15:23:44 +0100

    Testing completely like a black box, most simulating a Customer using the API. No checks in database, real API usage. Note: maybe this kind of testing could be also reuse to test: - Run stress test in this flows. - Monitor production essencial flow. (Think to run this test in production to confirm external services is running ok and the SLA that you aggreed) I saw moment to re-write this type of test twice (one app level and other production level). It just a reflection, I don't want to BDUF in the code right now, but I feel that this already happened.

--- test/todo.e2e-spec.ts
+++ test/todo.e2e-spec.ts
@@ -13,4 +13,69 @@ describe('TodoController (e2e)', () => {
     app = moduleFixture.createNestApplication();
     await app.init();
   });
+
+  it('/GET todos', () => {
+    return request(app.getHttpServer())
+      .get('/v1/todos')
+      .expect(200)
+      .expect({
+        data: [
+          { id: 1, title: 'Do the todo' , status: 'waiting'},
+          { id: 2, title: 'Do the todo 2' , status: 'working'},
+        ],
+      });
+  });
+
+  it('/POST todos', () => {
+    return request(app.getHttpServer())
+      .post('/v1/todos')
+      .expect(200)
+      .send({
+        data: [
+          { title: 'Do the todo' , status: 'waiting'},
+        ],
+      })
+      .then(response => {
+        expect(response.body.id).toBe(1);
+        expect(response.body.title).toBe('Do the todo');
+        expect(response.body.status).toBe('waiting');
+      });
+  });
+
+  it('/PUT todos', () => {
+    request(app.getHttpServer())
+      .get('/v1/todos/1')
+      .expect(200)
+      .then(response => {
+        expect(response.body.title).toBe('Do the todo');
+        expect(response.body.status).toBe('working');
+      });
+
+    return request(app.getHttpServer())
+      .put('/v1/todos/1')
+      .send(
+        {
+          status: 'waiting',
+        },
+      )
+      .expect(200)
+      .then(response => {
+        expect(response.body.title).toBe('Do the todo');
+        expect(response.body.status).toBe('waiting');
+      });
+  });
+
+  it('/DELETE todos', () => {
+    request(app.getHttpServer())
+      .delete('/v1/todos/1')
+      .expect(200);
+
+    return request(app.getHttpServer())
+      .get('/v1/todos/1')
+      .expect(404);
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
 });
